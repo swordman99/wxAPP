19956c5a951b816fd764369590c6934815d404df
swordman99 <39516706+swordman99@users.noreply.github.com>
2018-08-06 14:37:11 +0800

    change

--- manage.py
+++ manage.py
@@ -1,10 +1,19 @@
 from flask import Flask
+import json
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
     return '<h1>网站备案中</h1>'
+
+
+@app.route('/demo', methods=['GET'])
+def index():
+	t = {}
+	t['rank'] = '99'
+	return json.dumps(t, ensure_ascii=False)
+
 
 
 if __name__ == '__main__':
