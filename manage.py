2ffd2781d1bdbc149ea9c1a22ec83a647778890c
swordman99 <39516706+swordman99@users.noreply.github.com>
2018-08-07 13:51:54 +0800

    备案成功

--- manage.py
+++ manage.py
@@ -1,19 +1,33 @@
 from flask import Flask
+from flask import request
+import pymysql
+import os
 import json
+
+db = pymysql.connect('127.0.0.1', 'root', os.environ.get('MYSQL_PASSWORD'), 'demo')
+cursor = db.cursor()
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return '<h1>网站备案中</h1>'
+    s1 = '<h1>欢迎！</h1>'
+    s2 = '<a href="http://www.miitbeian.gov.cn">京ICP备18040938号</a>'
+    return s1 + s2
 
 
-@app.route('/home', methods=['GET'])
+@app.route('/home', methods=['POST'])
 def home():
-	t = {}
-	t['rank'] = '9'
-	return json.dumps(t, ensure_ascii=False)
-
+	data = request.get_json(force=True)
+	if data['id'] == '1':
+		t = {}
+		t['rank'] = '1'
+		return json.dumps(t, ensure_ascii=False)
+	else:
+		t = {}
+		t['rank'] = '0'
+		return json.dumps(t, ensure_ascii=False)
 
 
 if __name__ == '__main__':
